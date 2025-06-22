b4262403a5160034de052974b30a1ca33d06f04d
nxtnilesh <nxtnilesh@gmail.com>
2025-06-22 16:11:16 +0530

    f

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware()
 
-// git
+// gitxc
+// fdghf
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
