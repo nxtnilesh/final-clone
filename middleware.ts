337c542ecf56bc4415e8cbeed953622102739df6
nxtnilesh <nxtnilesh@gmail.com>
2025-06-22 16:05:59 +0530

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  publicRoutes: ["/","/board"],
-});
- 
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
+// git
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
